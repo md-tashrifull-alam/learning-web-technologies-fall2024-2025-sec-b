22dacc77d57e182f36f198370f658c1ff2523050
Md.Tashrifull Alam <tashrifrifat456@gmail.com>
2024-11-24 13:36:51 +0600

    Add files via upload

--- Midlabxm/login_cheak.php
+++ Midlabxm/login_cheak.php
@@ -1,16 +1,28 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $email = htmlspecialchars($_POST['email']);
-    echo "Email entered: " . $email;
-} else {
-    echo "No data received.";
-}
+    session_start();
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $password = htmlspecialchars($_POST['password']);
-    echo "Password entered: " . $password;
-} else {
-    echo "No data received.";
-}
+    if(isset($_POST['submit'])){
+        //echo "Test";
+        //print_r($_GET)
+        //$username = $_GET['username'];
+        $username = trim($_POST['username']);
+        $password = trim($_POST['password']);
+        //$username = $_REQUEST['username'];
 
+        //echo "Your username is: ". $username;
+        //echo "Your username is: {$username}";
+
+        if($username == null || empty($password)){
+            echo "Null username/password";
+        }else if($username == $password){
+            //echo "valid user!";
+
+            $_SESSION['xyz'] = true;
+            header('location: home.php');
+        }else{
+            echo "Invalid user!";
+        }
+    }else{
+        header('location: login.html');
+    }
 ?>
